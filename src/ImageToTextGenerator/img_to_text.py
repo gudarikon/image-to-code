6c17ef10b06337b90a41205554dabc3acdd37b4c
Arifkhanov Azamatkhan <aaarifkhanov@edu.hse.ru>
2023-01-17 01:32:33 +0300

    Made img_to_text more flexible

--- src/ImageToTextGenerator/img_to_text.py
+++ src/ImageToTextGenerator/img_to_text.py
@@ -1,24 +1,36 @@
+import json
 from pathlib import Path
 
 import click
-import numpy as np
-from paddleocr import PaddleOCR
+from PIL import Image
 
+from OCRProcessors.ocr_processor import OCRProcessor
 from utils import prepare_image
+import OCRProcessors as Op
 
 
-def paddle_img_to_text(img_path: Path):
-    ocr = PaddleOCR(use_angle_cls=True, lang="en")
-    image = prepare_image(img_path)
-    result = ocr.ocr(np.array(image), cls=False)[0]
-    texts = [line[1][0] for line in result]
-    texts = "\n".join(texts)
-    return texts
+def _get_processor(ocr_processor: str):
+    assert ocr_processor in Op.__all__, f"No given processor found. Try one of {Op.__all__}"
+    processor = getattr(Op, ocr_processor)
+    return processor
+
+
+def img_to_text(image: Image, ocr_processor: str, ocr_config: dict):
+    processor = _get_processor(ocr_processor)
+    processor_obj: OCRProcessor = processor(**ocr_config)
+    image = prepare_image(image)
+    text = processor_obj.process_image(image)
+    return text
 
 
 @click.argument("img_path", type=Path)
-def func_img_to_text(img_path: Path):
-    print(paddle_img_to_text(img_path))
+@click.argument("ocr_processor", type=str)
+@click.argument("path_to_ocr_config", type=Path)
+def func_img_to_text(img_path: Path, ocr_processor: str, path_to_ocr_config: Path):
+    image = Image.open(img_path)
+    with open(path_to_ocr_config, "r") as fr:
+        config = json.load(fr)
+    print(img_to_text(image, ocr_processor, config))
 
 
 f_img_to_text = click.command()(func_img_to_text)
