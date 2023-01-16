bb9af96a196f8f2e4f3ef3faff9f9fe7ee460586
Arifkhanov Azamatkhan <aaarifkhanov@edu.hse.ru>
2023-01-17 01:35:47 +0300

    Improved base for Text2Code function

--- src/TextToCodeGeneration/text_to_code.py
+++ src/TextToCodeGeneration/text_to_code.py
@@ -1,7 +1,22 @@
+from pathlib import Path
 from typing import List, Union
 
+import click
 from utils import preprocess_text
 
 
 def text_to_code(source_text: Union[str, List[str]]):
     source_text = preprocess_text(source_text)
+
+
+@click.argument("text_path", type=Path)
+def func_text_to_code(text_path: Path):
+    with open(text_path, "r") as f:
+        text = [line.rstrip() for line in f]
+    print(text_to_code(text))
+
+
+f_text_to_code = click.command()(func_text_to_code)
+
+if __name__ == "__main__":
+    f_text_to_code()
