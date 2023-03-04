031472dceb70ec75b8737f9380687e29d25b3e8d
Alex5041 <kononal@gmail.com>
2023-03-04 17:34:09 +0300

    add new data generator from code_search_net

---
+++ src/CodeToImageGenerator/dataset_parser.py
@@ -0,0 +1,63 @@
+import json
+from typing import TextIO
+
+from src.CodeToImageGenerator.main_old import MAX_SYMBOLS
+
+REPO_PATH = "C:\\Users\\alex\\Documents\\GitHub\\dataset_image_to_code\\src\\edu\\hse\\ru"
+JSONL_PATH = "C:\\Users\\alex\\Desktop\\java_test_0.jsonl"
+# maximum lines in function to make it possible to screenshot
+MAX_LINES = 28
+current_file_num = 1
+
+
+def create_file() -> TextIO:
+    global current_file_num
+
+    res = open(REPO_PATH + "\\Code{num}.java".format(num=current_file_num), "w", encoding="ascii")
+    res.write("package edu.hse.ru;\n\n")
+    res.write("class Code{num} ".format(num=current_file_num))
+    res.write("{\n")
+
+    current_file_num += 1
+    return res
+
+
+def write_code_if_fit(code: str, file: TextIO):
+    if code.find("void addOrder(String condition) {") != -1:
+        print(code)
+    if code.find("public FullEntity<?> save(final P pojo, final SaveContext ctx) {") != -1:
+        print(code)
+    if code.count("\n") + 1 > MAX_LINES:
+        return
+    lines = code.split("\r\n") if code.find("\r\n") != -1 else code.split("\n")
+    if len(list(filter(lambda x: len(x) > (MAX_SYMBOLS - 1), lines))) > 0:
+        return
+    try:
+        code.encode(encoding="ascii")
+        file.write("\n".join(lines))
+        file.write("\n\n")
+    except UnicodeEncodeError as e:
+        print(code)
+        return
+
+
+if __name__ == "__main__":
+    with open(JSONL_PATH, 'r') as json_file:
+        json_list = list(json_file)
+
+    i = 0
+    # files_num = 0
+    write_file = create_file()
+    for json_str in json_list:
+        if i == 100:
+            #  files_num += 1
+            # if files_num == 10:
+            #     break
+            write_file.write("}")
+            write_file.write("\n" * 30)
+            write_file = create_file()
+            i = 0
+        result = json.loads(json_str)
+        write_code_if_fit(result["code"], write_file)
+        i += 1
+    write_file.write("}")
--- src/CodeToImageGenerator/main.py
+++ src/CodeToImageGenerator/main.py
@@ -1,172 +1,47 @@
 import json
 from math import log10
 from pathlib import Path
-import platform
-import random
-from time import sleep
 from typing import List, Tuple
 
 import pyautogui
 
-from config import create_config, save_to_file
-from repo_parser import get_language, parse_repo
+from src.CodeToImageGenerator.config import create_config, save_to_file
+from src.CodeToImageGenerator.main_old import DIGIT_INC, GUTTER_BASE, LEFT_BOUND, \
+    LINE_HEIGHT, \
+    MAX_SYMBOLS, \
+    RIGHT_BOUND, \
+    SYMBOL_WIDTH, TOP_BOUND, change_visible_lines_number, \
+    change_visible_symbols, move_n_lines_rel, open_file, to_start_line
+from src.CodeToImageGenerator.repo_parser import parse_repo
 
-DIGIT_INC = 9.5
-GUTTER_BASE = 35
-
-MAX_SYMBOLS = 160  # 177
-MIN_SYMBOLS = 10
-SYMBOL_WIDTH = 10
-LINE_HEIGHT = 27.5
-
-TOP_BOUND = 106
-BOTTOM_BOUND = 940
-RIGHT_BOUND = 1895  # 26
-LEFT_BOUND = 64
-
-spaces_in_tab = 4
 config = create_config()
 
 
-def open_file(path: str):
-    if platform.system == "Darwin":
-        pyautogui.hotkey("cmd", "shift", "o")
-    else:
-        pyautogui.hotkey("ctrl", "shift", "n")
-    pyautogui.typewrite(path, interval=0.05)
-    pyautogui.hotkey("enter")
-    sleep(0.5)
+def save_screenshots(lines_num: int, max_symbols: int):
+    pyautogui.screenshot(
+        Path(config.screenshot_code_folder) / (str(config.id) + ".png"),
+        region=calculate_bounds_updated(lines_num, max_symbols))
 
 
-def to_start_line():
-    """
-    Move caret to `line_number`, that will be displayed as the top line of visible code.
-    1. Move to (`line_number` + `visible_lines_number`) number
-    2. Move caret (`visible_lines_number` - 1) up. That way `line_number` will be the topmost line
-    """
-    # scroll is unreliable because it is different from time to time
-    pyautogui.hotkey("ctrl", "home")
-
-
-def move_n_lines_rel(lines_number: int):
-    """
-    Move n lines down from this relative position
-    :param lines_number: number of moved lines
-    """
-    for _ in range(lines_number):
-        pyautogui.hotkey("down")
-
-
-def change_visible_lines_number(num_lines=30):
-    """
-    Changing visible lines number is achieved by moving terminal window up.
-    :param num_lines: number of lines that will be visible after. Maximal value is 30.
-    """
-    if config.visible_lines == -1:
-        pyautogui.mouseDown(x=400, y=BOTTOM_BOUND, button='left')
-    else:
-        pyautogui.mouseDown(x=400, y=config.visible_lines * LINE_HEIGHT + TOP_BOUND, button='left')
-    pyautogui.moveTo(400, TOP_BOUND, 0.1)
-    pyautogui.moveTo(400, TOP_BOUND + LINE_HEIGHT * num_lines, 0.1)
-    pyautogui.mouseUp(button='left')
-    config.visible_lines = num_lines
-
-
-def save_screenshots(lines_num: int):
-    pyautogui.screenshot(Path(config.screenshot_folder) / (str(config.id) + ".png"))
-    pyautogui.screenshot(Path(config.screenshot_code_folder) / (str(config.id) + ".png"),
-                         region=calculate_bounds(lines_num))
-
-
-def save_file(file_path: str, total_lines: int):
-    """
-    Parse file into screenshots and text from these screens
-    :param file_path: path to parsed file
-    :param total_lines: lines in that file
-    """
-    open_file(file_path)
-    to_start_line()
-    with open(file_path, 'r') as traversed_file:
-        caret_line = 1
-        lines = traversed_file.readlines()
-        stops = create_random_screen_stops(total_lines)
-
-        for i, stop in enumerate(stops):
-            if i == 0:
-                move_n_lines_rel(stop - caret_line + config.visible_lines - 2)
-            else:
-                move_n_lines_rel(stop - caret_line)
-            caret_line = stop
-            print(caret_line)
-            sleep(1)
-            text, is_trimmed = process_text(lines[stop - 1:stop + config.visible_lines - 1])
-            if not is_trimmed:
-                with open(Path(config.code_folder) / (str(config.id) + ".json"),
-                          'w') as code_file:
-                    code_file.write(
-                        json.dumps({"code": text, "language": get_language(file_path)}))
-                save_screenshots(total_lines)
-                config.id += 1
-
-
-def create_random_screen_stops(total_lines: int):
-    """
-    Create random lines where screenshot will be taken
-    :param total_lines: lines in file
-    """
-    res = []
-    current = random.randint(1, config.visible_lines)
-    while current + config.visible_lines < total_lines:
-        res.append(current)
-        current += random.randint(config.visible_lines, 2 * config.visible_lines)
-    print(res[:len(res) - 1])
-    return res[:len(res) - 1]
-
-
-def process_text(lines: List[str]) -> tuple:
-    """
-    Replace tabs with spaces, convert list to string
-    :param lines: list of processed lines
-    :return: and bool for trimmed symbols
-    """
-    res = []
-    is_trimmed = False
-    for line in lines:
-        no_tabs = line.replace("\t", " " * spaces_in_tab)[:len(line) - 1]
-        if len(no_tabs) >= config.visible_symbols:
-            is_trimmed = True
-        res.append(no_tabs)
-    return res, is_trimmed
-
-
-def change_visible_symbols(num_symbols: int = 30, lines_num: int = 100):
-    if config.visible_symbols == -1:
-        pyautogui.mouseDown(LEFT_BOUND, 200, button="left")
-        pyautogui.moveRel(
-            (RIGHT_BOUND - LEFT_BOUND)  # move from right to left
-            - log10(lines_num) * DIGIT_INC - GUTTER_BASE  # not counting gutter
-            - (num_symbols + 1) * SYMBOL_WIDTH, 0, 0.1)  # not counting num_symbols
-        pyautogui.mouseUp(button='left')
-        config.visible_symbols = num_symbols
-        #
-
-
-def calculate_bounds(lines_num: int) -> Tuple[float, float, float, float]:
+def calculate_bounds_updated(lines_num: int, max_symbols: int) -> Tuple[
+    float, float, float, float]:
     """
     Calculate bounds of code rectangle
     :return: left, top, width, height of code rectangle
     """
-    return RIGHT_BOUND - (config.visible_symbols + 1) * SYMBOL_WIDTH, \
+    return RIGHT_BOUND - (MAX_SYMBOLS + 1 - 3) * SYMBOL_WIDTH, \
            TOP_BOUND, \
-           (config.visible_symbols + 1) * SYMBOL_WIDTH, \
-           config.visible_lines * LINE_HEIGHT
+           (max_symbols + 1 - 4) * SYMBOL_WIDTH, \
+           (lines_num if lines_num != -1 else config.visible_lines) * LINE_HEIGHT
 
 
-def traverse_file(file_path: str, total_lines: int):
-    change_visible_lines_number(random.randint(10, 28))
-    x, y = pyautogui.position()
-    save_file(file_path, total_lines)
-    return x, y
+def change_visible_symbols_back(num_symbols: int = 30, lines_num: int = 100):
+    delta_x = (RIGHT_BOUND - LEFT_BOUND) - log10(lines_num) * DIGIT_INC - GUTTER_BASE - (
+            num_symbols + 1) * SYMBOL_WIDTH
+    pyautogui.mouseDown(LEFT_BOUND + delta_x, 200, button="left")
+    pyautogui.moveRel(-delta_x, 0, 0.1)
+    pyautogui.mouseUp(button='left')
+    config.visible_symbols = num_symbols
 
 
 def traverse_repo():
@@ -175,18 +50,72 @@ def traverse_repo():
         config.visited_files = set()
     for key in config.visited_files:
         config.repo_files.pop(key, None)
+
+    change_visible_lines_number(29)
+
     for file in config.repo_files:
-        change_visible_symbols(MAX_SYMBOLS, config.repo_files[file])
-        x, y = traverse_file(file, config.repo_files[file])
+        print(file)
+        if config.id > 3000:
+            print("Stop")
+            break
+        change_visible_symbols(MAX_SYMBOLS, config.repo_files[file], should_change=True)
+        traverse_file_for_functions(file, config.repo_files[file])
+        change_visible_symbols_back(MAX_SYMBOLS, config.repo_files[file])
+        pyautogui.sleep(0.1)
         config.visited_files.add(file)
-        newx, newy = pyautogui.position()
-        if abs(x - newx) > 10 or abs(y - newy) > 10:
-            config.visited_files = list(config.visited_files)
-            save_to_file(config.__dict__, "config.json")
-            return
     config.visited_files = list(config.visited_files)
+
     save_to_file(config.__dict__, "config.json")
 
 
+def traverse_file_for_functions(file_path: str, total_lines: int):
+    open_file(file_path)
+    to_start_line()
+    functions = get_function_lengths(file_path)
+
+    move_n_lines_rel(30)
+    for func, max_symbols in functions:
+        # print(length)
+        pyautogui.sleep(0.2)
+        save_screenshots(len(func), max_symbols)
+        save_code_to_file(func)
+        move_n_lines_rel(len(func) + 1)
+        config.id += 1
+
+
+def save_code_to_file(code: str):
+    with open(Path(config.code_folder) / (str(config.id) + ".json"), 'w') as code_file:
+        code_file.write(json.dumps({"code": code, "language": "java"}))
+
+
+def get_function_lengths(file_path) -> List[tuple]:
+    with open(file_path, "r") as file:
+        lines = file.readlines()[3:]
+    res = []
+    max_symbols = 0
+    prev_line = ""
+    # current_func_size = 0
+    current_func = []
+    for i in range(0, len(lines)):
+        line = lines[i]
+        current_func.append(line)
+        max_symbols = max(max_symbols, len(line))
+        if prev_line == "    }\n" and line == "\n":
+            res.append((current_func[:-1], max_symbols))
+            current_func = []
+            # current_func_size = -1
+            max_symbols = 0
+        # current_func_size += 1
+        prev_line = line
+    return res
+
+
 if __name__ == "__main__":
+    # pyautogui.sleep(3)
+    # pyautogui.moveTo(LEFT_BOUND, BOTTOM_BOUND, 0.1)
+    pyautogui.hotkey("alt", "tab")
+    pyautogui.sleep(1)
+    # change_visible_symbols(MAX_SYMBOLS)
+    # pyautogui.sleep(0.2)
+    # change_visible_symbols_back(MAX_SYMBOLS)
     traverse_repo()
