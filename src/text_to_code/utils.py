46fadfa9e2b1f4dac22d30db0b62b3d0262d38ed
Arifkhanov Azamatkhan <aaarifkhanov@edu.hse.ru>
2023-03-12 17:41:11 +0300

    added feature to preprocess_text

--- src/text_to_code/utils.py
+++ src/text_to_code/utils.py
@@ -1,7 +1,10 @@
 from typing import List, Union
 
 
-def preprocess_text(source_text: Union[str, List[str]]) -> str:
+def preprocess_text(source_text: Union[str, List[str]], strip_lines: bool = True) -> str:
     if isinstance(source_text, list):
-        source_text = "\n".join([s.lstrip() for s in source_text if not s.isspace() and len(s) > 0])
+        if strip_lines:
+            source_text = "\n".join([s.lstrip() for s in source_text if not s.isspace() and len(s) > 0])
+        else:
+            source_text = "".join([s for s in source_text if not s.isspace() and len(s) > 0])
     return source_text
