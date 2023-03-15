6086dcb7a1d254d99e5943507903179cc7d8bd92
Alex5041 <kononal@gmail.com>
2023-03-15 17:49:01 +0300

    fixed config paths

--- src/code_to_image/main_code_blocks.py
+++ src/code_to_image/main_code_blocks.py
@@ -9,8 +9,8 @@ from typing import List, Tuple
 
 import pyautogui
 
-from src.code_to_image.repo_parser import get_language, parse_repo
 from src.code_to_image.config_builder import ConfigBuilder
+from src.code_to_image.repo_parser import get_language, parse_repo
 
 DIGIT_INC = 9.5
 GUTTER_BASE = 35
@@ -30,7 +30,7 @@ config = ConfigBuilder().get_config()
 
 
 def open_file(path: str):
-    if platform.system == "Darwin":
+    if platform.system() == "Darwin":
         pyautogui.hotkey("cmd", "shift", "o")
     else:
         pyautogui.hotkey("ctrl", "shift", "n")
@@ -118,6 +118,7 @@ def assert_empty_folders():
         raise AssertionError("use empty folder for code jsons")
     if len(os.listdir(config.screenshot_folder)) != 0:
         raise AssertionError("use empty folder for screenshots")
+
 
 def create_random_screen_stops(total_lines: int):
     """
--- test/code_to_image/test_dataset_parser.py
+++ test/code_to_image/test_dataset_parser.py
@@ -5,12 +5,18 @@
   3. function file ends with line breaks to make possible
   moving caret down for screenshotting last functions
 """
+from pathlib import Path
+
+from src.code_to_image.config import Config
 from src.code_to_image.dataset_parser import *
 
 
 def test_parsing(dataset_parser_config_path):
     dataset_parser = DatasetParser()
-    dataset_parser.config = ConfigBuilder().get_config(str(dataset_parser_config_path))
+    dataset_parser.config = Config()
+    dataset_parser.config.code_search_functions_path = \
+        str(Path(__file__).parent.parent / "resources" / "java_test_functions.jsonl")
+
     functions = dataset_parser.parse_functions_into_files(functions_num_in_file=29,
                                                           in_memory=True)
 
