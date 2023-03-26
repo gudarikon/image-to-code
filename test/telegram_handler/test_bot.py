6d53404ebc9f1eb47aef12daa1740fc8b70798ae
MassterMax <59767630+MassterMax@users.noreply.github.com>
2023-03-27 01:02:10 +0300

    Update test_bot.py

--- test/telegram_handler/test_bot.py
+++ test/telegram_handler/test_bot.py
@@ -16,4 +16,5 @@ async def test_show_hello():
     name = "masstermax"
     message_mock = AsyncMock(from_user=User(name))
     await show_hello_handler(message=message_mock)
-    message_mock.answer.assert_called_with(f"Hello, {name}! Send me an image and I will extract code from it!:)")
+    expected = f"Hello, {name}! Send me an image and I will extract code from it!:)"
+    assert expected == message_mock.answer.text
