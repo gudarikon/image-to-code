835c938f864c592552098b090b7ec7a1f52d46a2
MassterMax <59767630+MassterMax@users.noreply.github.com>
2023-03-27 01:42:33 +0300

    Update test_bot.py

--- test/telegram_handler/test_bot.py
+++ test/telegram_handler/test_bot.py
@@ -19,6 +19,7 @@ async def test_show_hello():
     await show_hello_handler(message=message_mock)
     expected = f"Hello, {name}! Send me an image and I will extract code from it!:)"
     logging.info(message_mock.return_value)
+    logging.info(message_mock.return_value())
     logging.info(dir(message_mock.return_value))
     logging.info(vars(message_mock.return_value))
     assert expected == message_mock.answer.text
