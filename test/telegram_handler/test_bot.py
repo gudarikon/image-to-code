cbd43ae4717f487c081174a30a59196961a23299
MassterMax <59767630+MassterMax@users.noreply.github.com>
2023-03-27 00:38:32 +0300

    Update test_bot.py

--- test/telegram_handler/test_bot.py
+++ test/telegram_handler/test_bot.py
@@ -1,8 +1,9 @@
-import pytest
-
 from dataclasses import dataclass
 from unittest.mock import AsyncMock
-from src.telegram_handler.bot import show_hello
+
+import pytest
+
+from src.telegram_handler.hadlers import show_hello_handler
 
 
 @dataclass
@@ -14,5 +15,5 @@ class User:
 async def test_show_hello():
     name = "masstermax"
     message_mock = AsyncMock(from_user=User(name))
-    await show_hello(message=message_mock)
+    await show_hello_handler(message=message_mock)
     message_mock.answer.assert_called_with(f"Hello, {name}! Send me an image and I will extract code from it!:)")
