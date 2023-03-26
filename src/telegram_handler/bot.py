e968382b1fa798fa61e950ed00e536bff4a520e0
MassterMax <59767630+MassterMax@users.noreply.github.com>
2023-03-27 00:31:04 +0300

    split handlers from telegram bot

--- src/telegram_handler/bot.py
+++ src/telegram_handler/bot.py
@@ -1,12 +1,9 @@
 import logging
 import os
-from pathlib import Path
-from uuid import uuid4
 
 from aiogram import Bot, Dispatcher, executor, types
-from PIL import Image
 
-from src.telegram_handler import img_to_code
+from .hadlers import photo_handler, show_hello_handler
 
 BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
 
@@ -18,38 +15,12 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
 async def show_hello(message: types.Message):
-    await message.answer(
-        text=f"Hello, {message.from_user.username}! Send me an image and I will extract code from it!:)")
+    await show_hello_handler(message)
 
 
 @dp.message_handler(content_types=["photo"])
-async def handle_photo(message):
-    photo_size: types.PhotoSize = message.photo[-1]
-    file_info = await bot.get_file(photo_size.file_id)
-    file_ext = file_info.file_path.split(".")[-1]
-    # todo which name prefer?
-    # file_name = f"{photo_size.file_unique_id}.{file_ext}"
-    file_name = f"{uuid4()}.{file_ext}"
-    await message.photo[-1].download(file_name)
-    logging.info(f"downloaded: {file_name}")
-
-    file_path = Path(file_name)
-    logging.info(f"file path: {file_path}")
-
-    image = Image.open(file_path)
-    logging.info(image)
-
-    ocr_text, code = img_to_code(image, return_ocr_result=True)
-
-    text = "\n\n".join(["ocr text:", f"`{ocr_text}`", "code:", f"`{code}`"])
-    logging.info(text)
-    await message.answer(text=text, parse_mode="Markdown", reply=True)
-
-    # delete image
-    try:
-        os.remove(file_path)
-    except FileNotFoundError:
-        logging.warning(f"file to delete was not found: {file_path}")
+async def handle_photo(message: types.Message):
+    await photo_handler(message)
 
 
 if __name__ == "__main__":
--- src/telegram_handler/hadlers.py
+++ src/telegram_handler/hadlers.py
@@ -1,18 +1,24 @@
+import logging
+import os
+from pathlib import Path
+from uuid import uuid4
 
-# TODO read https://github.com/aiogram/aiogram/issues/378
+from aiogram import types
+from PIL import Image
+
+from src.telegram_handler import img_to_code
 
 
-async def show_hello_handler(message):
-     await message.answer(
+async def show_hello_handler(message: types.Message):
+    await message.answer(
         text=f"Hello, {message.from_user.username}! Send me an image and I will extract code from it!:)")
 
 
-async def photo_handler(message):
+async def photo_handler(message: types.Message):
     photo_size: types.PhotoSize = message.photo[-1]
-    file_info = await bot.get_file(photo_size.file_id)
+    file_info = await message.bot.get_file(photo_size.file_id)
     file_ext = file_info.file_path.split(".")[-1]
-    # todo which name prefer?
-    # file_name = f"{photo_size.file_unique_id}.{file_ext}"
+
     file_name = f"{uuid4()}.{file_ext}"
     await message.photo[-1].download(file_name)
     logging.info(f"downloaded: {file_name}")
