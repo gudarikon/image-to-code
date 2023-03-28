b5c2ddd3001c5ed239eefecace0c156e85072b9f
Arifkhanov Azamatkhan <aaarifkhanov@edu.hse.ru>
2023-03-28 21:26:32 +0300

    upgraded unify_path

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,13 @@
 from os import sep
 from pathlib import Path
 
+
 def unify_path(str_path: str) -> str:
     if sep == "\\":
         return str_path.replace("/", "\\")
     if sep == "/":
         return str_path.replace("\\", "/")
-    assert ValueError("Unknown OS file delimiter")
+    raise ValueError("Unknown OS file delimiter")
 
 
 def get_project_path() -> Path:
