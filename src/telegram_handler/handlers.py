5e858885dbd533da6a1653efd5b36bc96ae84f90
Arifkhanov Azamatkhan <aaarifkhanov@edu.hse.ru>
2023-03-28 19:46:25 +0300

    added temporary storage folder

--- src/telegram_handler/handlers.py
+++ src/telegram_handler/handlers.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from pathlib import Path
+import tempfile
 from uuid import uuid4
 
 from aiogram import types
@@ -18,15 +19,16 @@ async def photo_handler(message: types.Message):
     photo_size: types.PhotoSize = message.photo[-1]
     file_info = await message.bot.get_file(photo_size.file_id)
     file_ext = file_info.file_path.split(".")[-1]
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        dir_path = Path(tmpdirname)
+        file_name = f"{uuid4()}.{file_ext}"
+        await photo_size.download(destination_file=dir_path / file_name)
+        logging.info(f"downloaded: {file_name}")
 
-    file_name = f"{uuid4()}.{file_ext}"
-    await photo_size.download(file_name)
-    logging.info(f"downloaded: {file_name}")
+        file_path = dir_path / file_name
+        logging.info(f"file path: {file_path}")
 
-    file_path = Path(file_name)
-    logging.info(f"file path: {file_path}")
-
-    image = Image.open(file_path)
+        image = Image.open(file_path).copy()
     logging.info(image)
 
     ocr_text, code = img_to_code(image, return_ocr_result=True)
@@ -34,9 +36,3 @@ async def photo_handler(message: types.Message):
     text = "\n\n".join(["ocr text:", f"`{ocr_text}`", "code:", f"`{code}`"])
     logging.info(text)
     await message.answer(text=text, parse_mode="Markdown", reply=True)
-
-    # delete image
-    try:
-        os.remove(file_path)
-    except FileNotFoundError:
-        logging.warning(f"file to delete was not found: {file_path}")
