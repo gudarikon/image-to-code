639c5864b516e47df47a63ad48354bf8aa5794be
Arifkhanov Azamatkhan <aaarifkhanov@edu.hse.ru>
2023-03-28 21:53:55 +0300

    improved datasets tests

--- test/image_to_text/test_generate_dataset.py
+++ test/image_to_text/test_generate_dataset.py
@@ -11,7 +11,7 @@ from src.image_to_text.generate_dataset import func_generate_dataset
         ("images_folder_path", "PaddleProcessor", "paddleocr_config_path", False)
     ]
 )
-def test_img_to_text(images_folder_path_obj, ocr_name, ocr_config_path, add_spaces, tmp_path, request):
+def test_func_generate_dataset(images_folder_path_obj, ocr_name, ocr_config_path, add_spaces, tmp_path, request):
     images_folder_path_obj = request.getfixturevalue(images_folder_path_obj)
     ocr_config_path = request.getfixturevalue(ocr_config_path)
 
@@ -20,5 +20,5 @@ def test_img_to_text(images_folder_path_obj, ocr_name, ocr_config_path, add_spac
     for file in (os.listdir(images_folder_path_obj)):
         file_path = (tmp_path / (file.split(".")[0] + ".txt"))
         assert file_path.exists()
-        with open(file_path) as f:
+        with open(file_path, encoding="utf-16") as f:
             assert len(f.read().strip()) > 0
--- test/text_to_code/test_prepare_dataset.py
+++ test/text_to_code/test_prepare_dataset.py
@@ -3,7 +3,7 @@ import pandas as pd
 from src.text_to_code.prepare_dataset import prepare_dataset
 
 
-def test_img_to_text(images_folder_path, codes_folder_path, ocr_preds_folder_path,  tmp_path):
+def test_prepare_dataset(images_folder_path, codes_folder_path, ocr_preds_folder_path,  tmp_path):
 
     prepare_dataset(images_folder_path, codes_folder_path, ocr_preds_folder_path, tmp_path / "table.csv")
 
