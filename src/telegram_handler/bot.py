b6dd023213835ee1b9c030aed7c9216ab74f1642
Maxim <maxmail877@gmail.com>
2023-03-31 01:13:25 +0300

    playing with document_handler

--- src/telegram_handler/bot.py
+++ src/telegram_handler/bot.py
@@ -3,7 +3,7 @@ import os
 
 from aiogram import Bot, Dispatcher, executor, types
 
-from src.telegram_handler.handlers import photo_handler, show_hello_handler
+from src.telegram_handler.handlers import photo_handler, show_hello_handler, document_handler
 
 BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
 
@@ -22,18 +22,10 @@ async def show_hello(message: types.Message):
 async def handle_photo(message: types.Message):
     await photo_handler(message)
 
+
 @dp.message_handler(content_types=["document"])
 async def handle_file(message: types.Message):
-    file_id = message.document.file_id
-    print(dir(message.document))
-    print(vars(message.document))
-    print(message.document.mime_base)
-    logging.debug(dir(message.document))
-    logging.debug(vars(message.document))
-    file = await bot.get_file(file_id)
-    file_path = file.file_path
-    await message.answer(text=f"file path is {file_path} :)")
-    # await bot.download_file(file_path, "text.txt")
+    await document_handler(message)
 
 
 if __name__ == "__main__":
--- src/telegram_handler/handlers.py
+++ src/telegram_handler/handlers.py
@@ -38,3 +38,33 @@ async def photo_handler(message: types.Message):
     text = f"{text}code:\n\n`{code}`"
     logging.info(text)
     await message.answer(text=text, parse_mode="Markdown", reply=True)
+
+
+async def document_handler(message: types.Message):
+    file_id = message.document.file_id
+    if message.document.mime_base != "image":
+        await message.answer(text="Sorry, I do not understand you:(", reply=True)
+    file = await bot.get_file(file_id)
+    tg_file_path = file.file_path
+    file_ext = tg_file_path.split(".")[-1]
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        dir_path = Path(tmpdirname)
+        file_name = f"{uuid4()}.{file_ext}"
+        await message.bot.download_file(tg_file_path, dir_path / file_name)
+        logging.info(f"downloaded: {file_name}")
+
+        file_path = dir_path / file_name
+        logging.info(f"file path: {file_path}")
+
+        image = Image.open(file_path).copy()
+    logging.info(image)
+
+    ocr_text, code = img_to_code(image, return_ocr_result=False)
+
+    text = ""
+    if ocr_text is not None:
+        text = f"ocr text:\n\n`{ocr_text}`\n\n"
+    text = f"{text}code:\n\n`{code}`"
+    logging.info(text)
+    await message.answer(text=text, parse_mode="Markdown", reply=True)
